64715c689331b0b47fdca05311e604eec3d23c4b
LZ <ge94zeg@mytum.de>
2024-03-21 07:34:51 +0100

    dashboard view added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,11 +11,62 @@ export default function Page() {
     "https://de.wikipedia.org/wiki/bat",
     "https://de.wikipedia.org/wiki/fox",
     "https://de.wikipedia.org/wiki/squirrel",
+    "https://de.wikipedia.org/wiki/horse",
+    "https://de.wikipedia.org/wiki/cow",
+    "https://de.wikipedia.org/wiki/lizard",
   ];
+  const firstRow = (
+    <>
+      <div className="grid grid-cols-12 min-h-96">
+        <div className="p-2 col-span-7 bg-black">
+          <KibanaIframe src={srcs[0]} />
+        </div>
+        <div className="p-2 col-span-5 bg-red-700">
+          <KibanaIframe src={srcs[1]} />
+        </div>
+      </div>
+    </>
+  );
+  const secondRow = (
+    <>
+      <div className="grid grid-cols-12 min-h-96">
+        <div className="p-2 col-span-6 bg-amber-300">
+          <KibanaIframe src={srcs[2]} />
+        </div>
+        <div className="col-span-6 grid grid-cols-12">
+          <div className="p-2 col-span-6 bg-orange-800">
+            <KibanaIframe src={srcs[3]} />
+          </div>
+          <div className="p-2 col-span-6 bg-lime-900">
+            <KibanaIframe src={srcs[4]} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+  const thirdRow = (
+    <>
+      <div className="grid grid-cols-12 min-h-96">
+        <div className="col-span-6 grid grid-cols-12">
+          <div className="p-2 col-span-6 bg-orange-800">
+            <KibanaIframe src={srcs[5]} />
+          </div>
+          <div className="p-2 col-span-6 bg-lime-900">
+            <KibanaIframe src={srcs[6]} />
+          </div>
+        </div>
+        <div className="p-2 col-span-6 bg-amber-300">
+          <KibanaIframe src={srcs[7]} />
+        </div>
+      </div>
+    </>
+  );
   return (
     <>
-      <div className="mt-5 container">
-        <h1>dashboard</h1>
+      <div className="mt-5 container flex flex-col gap-2">
+        {firstRow}
+        {secondRow}
+        {thirdRow}
       </div>
     </>
   );
