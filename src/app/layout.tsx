919d8454f6c4bb42f9d74b10a0db532a5d146131
LZ <ge94zeg@mytum.de>
2024-03-21 09:52:47 +0100

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "G20",
   description: "",
